c14c31697023866b447c5da7017fc433cfdd5665
Alexander <alex.prennsberger@gmail.com>
2012-10-19 20:20:00 +0200

    Test für ArchivList
    Find in Fehler nicht und scheiss drauf...morgen gehts weiter

--- src/prenns/ue1/test/Main.java
+++ src/prenns/ue1/test/Main.java
@@ -31,21 +31,21 @@ public class Main {
 
 	public static void EventTest(MusikGruppe grp) throws ParseException {
 
-		grp.newEvent(new Auftritt("Wien", format.parse("03.10.1991"),
-				format.parse("03.11.1991 00:00"), 12.00));
+		grp.newEvent(new Auftritt("Wien", format.parse("03.10.1991"), format
+				.parse("03.11.1991 00:00"), 12.00));
 
-		grp.newEvent(new Probe("Wopfing", format.parse("05.01.1991"),
-				format.parse("06.01.1991 00:00"), 12.00));
+		grp.newEvent(new Probe("Wopfing", format.parse("05.01.1991"), format
+				.parse("06.01.1991 00:00"), 12.00));
 
-		grp.newEvent(new Probe("Wopfing", format.parse("05.04.1991"),
-				format.parse("06.04.1991"), 12.00));
+		grp.newEvent(new Probe("Wopfing", format.parse("05.04.1991"), format
+				.parse("06.04.1991"), 12.00));
 
 		Collection<Event> events = new ArrayList<Event>();
 
 		events = grp.getEvents(format.parse("05.01.1991"),
 				format.parse("31.01.1992"), Event.class);
 
-		assert(events.size() == 3);
+		assert (events.size() == 3);
 	}
 
 	public static void ArchivTest(MusikGruppe grp) throws ParseException {
@@ -57,17 +57,17 @@ public class Main {
 		grp.newMember(m1, format.parse("03.10.1991"));
 		grp.newMember(m2, format.parse("06.11.2012"));
 		grp.newMember(m3, format.parse("22.05.1998"));
-		
+
 		Collection<Mitglied> members = new ArrayList<Mitglied>();
 		
-		boolean test = grp.removeMember(m2, format.parse("08.11.2013"));
-		
+		boolean test = grp.removeMember(m1, format.parse("08.11.2013"));
+
 		System.out.println(test);
-		
+
 		members = grp.getMembers();
-		
-		for(Mitglied m : members) {
-			
+
+		for (Mitglied m : members) {
+
 			System.out.println(m);
 		}
 
