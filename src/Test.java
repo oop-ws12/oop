a505d0100c634fed721c46484241bbae8dc5586b
Alexander <alex.prennsberger@gmail.com>
2012-11-19 13:25:45 +0100

    Test Pkt2 laut Angabe

--- src/Test.java
+++ src/Test.java
@@ -129,7 +129,99 @@ public class Test {
 
 
 	private static void testTimes() {
+		
+		//Map erstellen
 		OrderedMap<MeanElapsedTime, CompositeTime> m1 = new OrderedMap<MeanElapsedTime, CompositeTime>();
+		
+		//Sets erstellen
+		Set<Double> set1 = new Set<Double>();
+		Set<Double> set2 = new Set<Double>();
+		
+		//Messreihen erstellen
+		Double[] messwerte1 = new Double[] { 13.0, 14.2, 1.4, 0.5 };
+		Double[] messwerte2 = new Double[] { 4.0, 100.4, 20.9, 3.5, 80.2 };
+		Double[] messwerte3 = new Double[] { 4.6, 10.4, 220.9, 33.5, 8.2 };
+		
+		//Sets mit den Messreihen initialisieren
+		for(double d : messwerte1) {
+			set1.insert(d);
+		}
+		for(double d : messwerte2) {
+			set2.insert(d);
+		}
+		
+		//Zeiten erstellen
+		MeanElapsedTime[] times = new MeanElapsedTime[]	{
+				
+				new MeanElapsedTime(set1),
+				new MeanElapsedTime(set2)		
+		};
+		
+		//Map mit den Zeiten initialisieren
+		for(MeanElapsedTime t : times) {
+			m1.insert(t);
+		}
+		
+		//Objekte erzeugen und mit Messwerten initialisieren
+		CompositeTime[] comps = new CompositeTime[] {
+				
+				new CompositeTime(messwerte3),
+				new CompositeTime(messwerte1),	
+				new CompositeTime(messwerte2),
+		};
+		
+		//Iterator ueber die Map
+		MapIterator<MeanElapsedTime, CompositeTime> it = m1.iterator();
+		//Auf das erste Element der Map gehen
+		it.next();
+		//Iterator vom ersten Element ueber die Objekte 
+		InsertIterator<CompositeTime> insertTimes = it.iterator();
+		
+		//Objekte, auf welche das erste Element verweist, einfuegen
+		insertTimes.add(comps[0]);
+		insertTimes.add(comps[2]);
+		//Auf das zweite Element in der Map gehen
+		it.next();
+		//Iterator vom zweiten Element
+		insertTimes = it.iterator();
+		//Objekte, auf welche das zweite Element verweist, einfuegen
+		insertTimes.add(comps[1]);
+		
+		it = m1.iterator();
+		
+		//Erstes Element in der Map
+		MeanElapsedTime current = it.next();
+		
+		//Ausgabe des groessten Messwertes vom ersten Element MeanElapsedTime
+		eq(messwerte1[1], current.highestValue());
+		System.out.println(current.highestValue());
+		
+		//Iterator vom ersten Element ueber die CompositeTimes, zeigt auf das ZULETZT eingefuegte Element
+		insertTimes = it.iterator();
+				
+		//Erstes Objekt vom ersten Element in der Map
+		CompositeTime current2 = insertTimes.next();
+		
+		//Ausgabe der kurezesten Zeit des ersten Objekts
+		eq(messwerte2[3], current2.getShortestTime());
+		System.out.println(current2.getShortestTime());
+		
+		//Zweites Objekt vom ersten Element
+		current2 = insertTimes.next();
+		
+		eq(messwerte3[0], current2.getShortestTime());
+		System.out.println(current2.getShortestTime());
+		
+		current = it.next();
+		insertTimes = it.iterator();
+	
+		eq(messwerte2[1], current.highestValue());
+		System.out.println(current.highestValue());
+		
+		current2 = insertTimes.next();
+	
+		eq(messwerte1[3], current2.getShortestTime());
+		System.out.println(current2.getShortestTime());
 		
 	}
 
