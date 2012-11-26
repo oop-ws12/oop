9bfe8da0fb16378c629dfbd8cc879dd4e494c438
Lukas <steinbrecherluk@gmail.com>
2012-11-26 13:30:58 +0100

    Software

--- src/Leibwaechter.java
+++ src/Leibwaechter.java
@@ -1,5 +1,12 @@
 
 public class Leibwaechter extends Beschuetzer {
+	public static abstract class BasicSoftware extends Software<Leibwaechter> implements AndroidVerordnung {		
+		@Override
+		public boolean validate(Leibwaechter a) {
+			return getStufe() == 4;
+		}
+	}
+	
 	@Override
 	public <T> T visit(AndroidVisitor<T> v) {
 		return v.visit(this);
